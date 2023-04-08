2c78d95422e2c0d089d93c87af337298ffa6bbe9
Mahmoud Reda <mahmoudreda457@gmail.com>
2023-04-08 22:04:36 +0200

    add similar posts

--- djblog/blog/views.py
+++ djblog/blog/views.py
@@ -4,21 +4,22 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger  # new
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 
 # Create your views here.
-class PostListView(ListView):
-    """
-    Alternative post list view
-    """
+# class PostListView(ListView):
+#     """
+#     Alternative post list view
+#     """
 
-    queryset = Post.objects.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+#     queryset = Post.objects.all()
+#     context_object_name = "posts"
+#     paginate_by = 3
+#     template_name = "blog/post/list.html"
 
 
 def post_list(request, tag_slug=None):
@@ -55,10 +56,28 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list(
+        "id", flat=True
+    )  # [1, 2, ...] instead of  [(1,), (2,), ...]
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags",
+        "-publish",
+    )[
+        :4
+    ]  # only the first four posts
+
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
